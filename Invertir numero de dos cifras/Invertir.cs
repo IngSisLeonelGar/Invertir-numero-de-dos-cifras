0a4bca9d567c0cb91ff9f18b3b82a73cc8a294d8
Yankrlos <ing.leonelgarciaflo@gmail.com>
2024-05-13 22:35:41 -0600

    Agregar archivos de proyecto.

---
+++ Invertir numero de dos cifras/Invertir.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertir_numero_de_dos_cifras
+{
+    internal class Invertir
+    {
+        static void Main(String[] args)
+        {
+            int num, aux, dec, uni;
+            string linea;
+            Console.WriteLine("Ingrese numero de dos cifras: ");
+            linea = Console.ReadLine();
+            num = int.Parse(linea);
+            dec = num / 10;
+            uni = num % 10;
+            aux = (uni * 10) + dec;
+            Console.WriteLine("Numero invertido es: " + aux);
+            Console.WriteLine("Pulse una tecla: ");
+            Console.ReadLine();
+        }
+    }
+}
